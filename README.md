500be6d0a3607bb20a2c9079bcafcf4be89d42c8
jmons <johan.mons@viacesi.fr>
2024-11-19 09:42:50 +0100

    Modif readme

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# SDL_lib_v2
-
-## updates
-- new function init_game()
-- correct bug checkPos (drawRect)
-## Description main.c
-- Squelette du programme avec boucle pour gestion event et exemple  :
-  - Vous pouvez deplacer un carré latteralement avec Q | D
+Projet algorithmique - septembre 2024
--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
+/*
 int hauteur;
 int hauteurBranches;
 int hauteurTronc;
@@ -10,10 +10,11 @@ int hauteurPot;
   //sous fonctions de l'arbre
 int main (){
 //initialiser la variable de hauteur
-/*  printf("Indiquez la taille de votre sapin.");
-  scanf ("%d",&hauteur);*/
+  printf("Indiquez la taille de votre sapin.");
+  scanf ("%d",&hauteur);
   hauteur=50;
   hauteurBranches = hauteur*0.8;
   hauteurTronc= hauteur*0.2;
   printf("%d hauteur", hauteur, "%d branches", hauteurBranches, "%d tronc", hauteurTronc);
 }
+*/
